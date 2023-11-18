1b555da78f5e3741d40fad20b0b806c487f37986
Daniel <lopezcorreasdaniel@gmail.com>
2023-11-18 13:25:24 +0000

    class Weather

---
+++ src/main/java/org/daniel/model/Weather.java
@@ -0,0 +1,52 @@
+package org.daniel.model;
+
+import java.time.Instant;
+
+public class Weather {
+	private Instant ts;
+	private Location location;
+	private double clouds;
+	private double wind;
+	private double temperature;
+	private double precipitation;
+	private double humidity;
+
+	public Weather(Instant ts, Location location, double clouds, double wind, double temperature, double precipitation, double humidity){
+
+		this.ts = ts;
+		this.location = location;
+		this.clouds = clouds;
+		this.wind = wind;
+		this.temperature = temperature;
+		this.precipitation = precipitation;
+		this.humidity = humidity;
+	}
+
+	public Instant getTs() {
+		return ts;
+	}
+
+	public Location getLocation() {
+		return location;
+	}
+
+	public double getClouds() {
+		return clouds;
+	}
+
+	public double getWind() {
+		return wind;
+	}
+
+	public double getTemperature() {
+		return temperature;
+	}
+
+	public double getPrecipitation() {
+		return precipitation;
+	}
+
+	public double getHumidity() {
+		return humidity;
+	}
+}
