05a75aac2019c8c7c4d12a65d37d18540c5c27ab
Daniel <lopezcorreasdaniel@gmail.com>
2024-01-09 16:18:25 +0000

    XoteloAPIException revision commit

---
+++ hotel-provider/src/main/java/org/daniel/exception/XoteloAPIException.java
@@ -0,0 +1,9 @@
+package org.daniel.exception;
+
+import java.io.IOException;
+
+public class XoteloAPIException extends IOException {
+	public XoteloAPIException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
