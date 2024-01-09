96d235fc0fdd8977785a67faedd64a4e48bd533f
Daniel <lopezcorreasdaniel@gmail.com>
2024-01-09 13:59:47 +0000

    class Booking revision commit

---
+++ hotel-provider/src/main/java/org/daniel/model/Booking.java
@@ -0,0 +1,38 @@
+package org.daniel.model;
+
+import java.time.Instant;
+
+public class Booking {
+
+	private Instant ts;
+	private String ss;
+	private Hotel hotel;
+	private String arrival;
+	private String departure;
+	private String platformUsed;
+	private Double totalPrice;
+
+	public Booking(Hotel hotel, String arrival, String departure, String platformUsed, Double totalPrice) {
+		this.ts = Instant.now();
+		this.ss = "hotel-provider";
+		this.hotel = hotel;
+		this.arrival = arrival;
+		this.departure = departure;
+		this.platformUsed = platformUsed;
+		this.totalPrice = totalPrice;
+	}
+
+	@Override
+	public String toString() {
+		return "Booking{" +
+				"ts=" + ts +
+				", ss='" + ss + '\'' +
+				", hotel=" + hotel.getTitle() +
+				", arrival='" + arrival + '\'' +
+				", departure='" + departure + '\'' +
+				", platformUsed='" + platformUsed + '\'' +
+				", totalPrice=" + totalPrice +
+				'}';
+	}
+}
+
