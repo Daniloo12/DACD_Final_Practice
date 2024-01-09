5737d9cc3c2269afa58eb1c29f3619e49f50e42c
Daniel <lopezcorreasdaniel@gmail.com>
2024-01-09 18:55:08 +0000

    class Location revision commit

---
+++ travel-business-unit/src/main/java/org/daniel/model/Location.java
@@ -0,0 +1,19 @@
+package org.daniel.model;
+
+public class Location {
+	private String area;
+	private String location;
+
+	public Location(String area, String location) {
+		this.area = area;
+		this.location = location;
+	}
+
+	public String getArea() {
+		return area;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+}
