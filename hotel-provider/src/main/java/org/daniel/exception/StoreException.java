08dabc2362bc72e452533910bfde391a7fd38c13
Daniel <lopezcorreasdaniel@gmail.com>
2024-01-09 14:07:18 +0000

    class StoreException revision commit

---
+++ hotel-provider/src/main/java/org/daniel/exception/StoreException.java
@@ -0,0 +1,9 @@
+package org.daniel.exception;
+
+import javax.jms.JMSException;
+
+public class StoreException extends JMSException {
+	public StoreException(String reason) {
+		super(reason);
+	}
+}
